8afc34926b44e218a3c9f34f07a27e25b02bea0c
NishaNanda22 <60631971+NishaNanda22@users.noreply.github.com>
2020-07-25 16:25:31 -0400

    Add files via upload

--- homework.sql
+++ homework.sql
@@ -44,4 +44,74 @@ CREATE TABLE dept_emp (
     PRIMARY KEY (emp_no,dept_no)
 );
 
+SELECT  employees.emp_no,
+        employees.last_name,
+        employees.first_name,
+        employees.sex,
+        salaries.salary
+FROM employees 
+    LEFT JOIN salaries 
+    ON (employees.emp_no = salaries.emp_no)
+ORDER BY employees.emp_no;
+
+--between and and are inclusive?
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+SELECT  dept_manager.dept_no,
+        departments.dept_name,
+        dept_manager.emp_no,
+        employees.last_name,
+        employees.first_name
+FROM dept_manager 
+    INNER JOIN departments 
+        ON (dept_manager.dept_no = departments.dept_no)
+    INNER JOIN employees 
+        ON (dept_manager.emp_no = employees.emp_no);
+
+SELECT  employees.emp_no,
+        employees.last_name,
+        employees.first_name,
+        departments.dept_name
+FROM employees 
+    INNER JOIN dept_emp 
+        ON (employees.emp_no = dept_emp.emp_no)
+    INNER JOIN departments 
+        ON (dept_emp.dept_no = departments.dept_no)
+ORDER BY employees.emp_no;
+
+SELECT first_name, last_name, birth_date, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+SELECT  employees.emp_no,
+        employees.last_name,
+        employees.first_name,
+        departments.dept_name
+FROM employees 
+    INNER JOIN dept_emp 
+        ON (employees.emp_no = dept_emp.emp_no)
+    INNER JOIN departments 
+        ON (dept_emp.dept_no = departments.dept_no)
+WHERE departments.dept_name = 'Sales'
+ORDER BY employees.emp_no;
+
+SELECT  employees.emp_no,
+        employees.last_name,
+        employees.first_name,
+        departments.dept_name
+FROM employees 
+    INNER JOIN dept_emp 
+        ON (employees.emp_no = dept_emp.emp_no)
+    INNER JOIN departments 
+        ON (dept_emp.dept_no = departments.dept_no)
+WHERE departments.dept_name IN ('Sales', 'Development')
+ORDER BY employees.emp_no;
+
+SELECT last_name, COUNT(last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC;
 
